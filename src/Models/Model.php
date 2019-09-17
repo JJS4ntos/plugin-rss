9d8ebeb3c45ac4a7720187df99fd0b2a2ef967bb
jsantos.class <jsantos.class@gmail.com>
2019-09-17 13:28:34 -0400

    Instalação de testes unitários

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -1,6 +1,19 @@
 <?php
 namespace App\Models;
 
-class Model {
+class Model{
+  
+  protected $post_type;
+  protected $metas = [];
+  
+  public function __construct( $post_type = 'post' ) {
+    $this->post_type = $post;
+  }
+  
+  private function __call($func, $params){
+      if( in_array( $func, $this->metas ) ){
+          return 'Test'.$func;
+      }
+  }
   
 }
