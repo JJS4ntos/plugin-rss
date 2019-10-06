364dd078a3d0c19daa38787726b859f20172483f
Junior Santos <jsantos.class@gmail.com>
2019-10-06 16:55:44 +0000

    Cache folder fix bug

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/*
+  Plugin Name: Runcinator
+  Plugin URI: https://github.com/JJS4ntos
+  Description: Runcinator a framework to build simple and complex plugins WordPress.
+  Version: ALPHA
+  Author: Jair Júnior
+  Author URI: https://github.com/JJS4ntos
+*/
+// If this file is accessed directory, then abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+if ( !file_exists('src/cache') ) {
+  mkdir('src/cache');
+}
+
+define('PLUGIN_NAME', 'Runcinator');
+define('SD_PATH', plugin_dir_url( __FILE__ ));
+define('SD_PLUGIN_PATH', plugin_dir_path( __FILE__ ));
+define('URL_SCOPE', 'runcinator-api');
+
+require_once 'vendor/autoload.php';
+require_once 'src/Config/Setup.php';
+require_once 'src/database/install.php';
+
+require_once 'src/routes.php';
---
+++ src/Config/Setup.php
@@ -0,0 +1,87 @@
+<?php
+namespace App\Config;
+
+use App\Controllers\Controller;
+
+class Setup extends Controller{
+
+  private $icon = 'dashicons-image-filter';
+
+  /**
+   * Call register functions to register pages and assets
+   */
+  public function __construct() {
+    parent::__construct();
+    add_action('wp_enqueue_scripts', array($this, 'register_assets') );
+    add_action('admin_menu', array($this, 'page_setup') );
+    add_action('admin_enqueue_scripts', array($this, 'register_assets_admin') );
+  }
+
+  /**
+   * Register menu link and plugin page
+   * @param  string $icon [description]
+   * @return
+   */
+  public function page_setup(){
+    add_menu_page( PLUGIN_NAME, PLUGIN_NAME, 'manage_options', sanitize_key(PLUGIN_NAME), array($this, 'admin_page'), $this->icon, 3 );
+  }
+
+  /**
+   * Register all commons plugin assets
+   * @return
+   */
+  public function register_assets() {
+    $js_folder = SD_PLUGIN_PATH . '/src/assets/js/';
+    $css_folder = SD_PLUGIN_PATH . '/src/assets/css/';
+    $scripts = scandir($js_folder);
+    //var_dump($scripts);
+    $styles = scandir($css_folder);
+    foreach ($styles as $style) {
+      if( !is_dir($style) ) {
+        wp_enqueue_style( URL_SCOPE . mt_rand(0, 9000), '/wp-content/plugins/'.sanitize_key(PLUGIN_NAME).'/src/assets/css/' . $style);
+      }
+    }
+    foreach ($scripts as $script) {
+      if( !is_dir($script) ) {
+        wp_enqueue_script( URL_SCOPE . mt_rand(0, 9000), '/wp-content/plugins/'.sanitize_key(PLUGIN_NAME).'/src/assets/js/' . $script);
+      }
+    }
+  }
+
+  /**
+   * Register only assets that will be use in admin page
+   * @return
+   */
+  public function register_assets_admin() {
+    $js_folder = SD_PLUGIN_PATH . '/src/assets/admin/js/';
+    $css_folder = SD_PLUGIN_PATH . '/src/assets/admin/css/';
+    $scripts = scandir($js_folder);
+    //var_dump($scripts);
+    $styles = scandir($css_folder);
+    foreach ($styles as $style) {
+      if( !is_dir($style) ) {
+        wp_enqueue_style( URL_SCOPE . mt_rand(0, 9000), '/wp-content/plugins/'.sanitize_key(PLUGIN_NAME).'/src/assets/admin/css/' . $style);
+      }
+    }
+    foreach ($scripts as $script) {
+      if( !is_dir($script) ) {
+        wp_enqueue_script( URL_SCOPE . mt_rand(0, 9000), '/wp-content/plugins/'.sanitize_key(PLUGIN_NAME).'/src/assets/admin/js/' . $script);
+      }
+    }
+  }
+
+  /**
+   * Generate a view for admin page
+   * @return [type] [description]
+   */
+  public function admin_page() {
+     echo $this->generateView('index', []);
+  }
+
+}
+
+/**
+ * Automatically start plugin after index.php
+ * @var Setup
+ */
+$setup = new Setup();
